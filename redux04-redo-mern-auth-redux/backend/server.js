74e3d47037e2ef29730c91b6188f13f401fdad16
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-22 23:04:46 -0500

    created middleware

--- redux04-redo-mern-auth-redux/backend/server.js
+++ redux04-redo-mern-auth-redux/backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 const port = process.env.PORT || 4001;
 
 const app = express();
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
 app.listen(port, () => {
