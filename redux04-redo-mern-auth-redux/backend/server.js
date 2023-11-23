c79ecf5668384566465a114b567815a7ebc3fd55
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-23 07:42:29 -0500

    create user backend function

--- redux04-redo-mern-auth-redux/backend/server.js
+++ redux04-redo-mern-auth-redux/backend/server.js
@@ -11,6 +11,8 @@ dotenv.config();
 const port = process.env.PORT || 4001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
