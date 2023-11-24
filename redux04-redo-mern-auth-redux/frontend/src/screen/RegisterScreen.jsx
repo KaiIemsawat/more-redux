07742c14a981169776a28857b69727f9341ddb30
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-24 09:18:39 -0500

    signup function

--- redux04-redo-mern-auth-redux/frontend/src/screen/RegisterScreen.jsx
+++ redux04-redo-mern-auth-redux/frontend/src/screen/RegisterScreen.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
 
 const RegisterScreen = () => {
     const [name, setName] = useState("");
@@ -10,9 +15,43 @@ const RegisterScreen = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    // To get user data
+    // 'userInfo' comes from 'authSlice.js'
+    // 'auth' from '(state) => state.auth' also comes from 'authSlice.js'
+    const { userInfo } = useSelector((state) => state.auth);
+
+    // const [justNameOfFunctionToCall, {isLoading}] = useLoginMutation();
+    // {isLoading} is there by default
+    const [register, { isLoading }] = useRegisterMutation();
+
+    // If user is logged in
+    useEffect(() => {
+        if (userInfo) {
+            navigate("/");
+        }
+    }, [navigate, userInfo]);
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        console.log("submit");
+        if (password !== confirmPassword) {
+            toast.error("Passwords are not match");
+        } else {
+            try {
+                // calling 'register'
+                // 'unwrap()' will return 'promise'
+                const res = await register({ name, email, password }).unwrap();
+                // setting user to 'state' and 'local storage'
+                dispatch(setCredentials({ ...res }));
+                // navigate to home screen
+                navigate("/");
+            } catch (error) {
+                // console.error(error?.data?.message || error.error);
+                toast.error(error?.data?.message || error.error);
+            }
+        }
     };
     return (
         <FormContainer>
@@ -58,6 +97,9 @@ const RegisterScreen = () => {
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+
+                {isLoading && <Loader />}
+
                 <Button className="mt-3" type="submit" variant="primary">
                     Sign Up
                 </Button>
