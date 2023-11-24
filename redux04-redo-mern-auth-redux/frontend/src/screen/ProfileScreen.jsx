18ec50fb550aaa11573c03682498debb0e7200cb
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-24 10:10:38 -0500

    update user function

--- redux04-redo-mern-auth-redux/frontend/src/screen/ProfileScreen.jsx
+++ redux04-redo-mern-auth-redux/frontend/src/screen/ProfileScreen.jsx
@@ -1,5 +1,110 @@
+import { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { setCredentials } from "../slices/authSlice";
+import { useUpdateUserMutation } from "../slices/usersApiSlice";
+
+/* Update user details */
 const ProfileScreen = () => {
-    return <div>ProfileScreen</div>;
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    // To get user data
+    // 'userInfo' comes from 'authSlice.js'
+    // 'auth' from '(state) => state.auth' also comes from 'authSlice.js'
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const [updateProfile, { isLoading }] = useUpdateUserMutation();
+
+    useEffect(() => {
+        setName(userInfo.name);
+        setEmail(userInfo.email);
+    }, [userInfo.setName, userInfo.setEmail]);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            toast.error("Passwords are not match");
+        } else if (password.length < 6) {
+            toast.error("Passwords need to be at least 6 charactors");
+        } else {
+            try {
+                const res = await updateProfile({
+                    _id: userInfo._id,
+                    name,
+                    email,
+                    password,
+                }).unwrap();
+                dispatch(setCredentials({ ...res }));
+                toast.success("Profile Updated");
+            } catch (error) {
+                toast.error(error?.data?.message || error.error);
+            }
+        }
+    };
+    return (
+        <FormContainer>
+            <h1>Update Profile</h1>
+            <Form onSubmit={submitHandler}>
+                {/* Name */}
+                <Form.Group className="my-2" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                {/* Email */}
+                <Form.Group className="my-2" controlId="email">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                {/* Password */}
+                <Form.Group className="my-2" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                {/* Confirm Password */}
+                <Form.Group className="my-2" controlId="confirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                {isLoading && <Loader />}
+
+                <Button className="mt-3" type="submit" variant="primary">
+                    Update
+                </Button>
+            </Form>
+        </FormContainer>
+    );
 };
 
 export default ProfileScreen;
