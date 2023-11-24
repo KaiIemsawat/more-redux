bb5ec7979468cf992cc42fb6e4cf3dc84c9d8d17
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-24 08:06:11 -0500

    Logout

--- redux04-redo-mern-auth-redux/frontend/src/components/Header.jsx
+++ redux04-redo-mern-auth-redux/frontend/src/components/Header.jsx
@@ -2,10 +2,29 @@ import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 const Header = () => {
     // userInfo comes from 'authSlice.js'
     const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <header>
@@ -28,7 +47,9 @@ const Header = () => {
                                                 Profile
                                             </NavDropdown.Item>
                                         </LinkContainer>
-                                        <NavDropdown.Item>
+                                        <NavDropdown.Item
+                                            onClick={logoutHandler}
+                                        >
                                             Logout
                                         </NavDropdown.Item>
                                     </NavDropdown>
