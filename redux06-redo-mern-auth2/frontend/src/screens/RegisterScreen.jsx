7b0d00d0de9cdd801b4d31d66ce23be9f3fe0ba3
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-19 20:50:05 -0500

    Register form, Routed

--- redux06-redo-mern-auth2/frontend/src/screens/RegisterScreen.jsx
+++ redux06-redo-mern-auth2/frontend/src/screens/RegisterScreen.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 
 import FormContainer from "../components/FormContainer";
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -14,8 +16,19 @@ const LoginScreen = () => {
     };
     return (
         <FormContainer>
-            <h1>Sign In</h1>
+            <h1>Sign Up</h1>
             <Form onSubmit={submitHandler}>
+                {/* Name */}
+                <Form.Group className="my-2" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="name"
+                        value={name}
+                        onChange={() => setName(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                {/* Email */}
                 <Form.Group className="my-2" controlId="email">
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
@@ -25,6 +38,7 @@ const LoginScreen = () => {
                         onChange={() => setEmail(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+                {/* Password */}
                 <Form.Group className="my-2" controlId="password">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
@@ -34,14 +48,23 @@ const LoginScreen = () => {
                         onChange={() => setPassword(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+                <Form.Group className="my-2" controlId="confirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="confirm password"
+                        value={confirmPassword}
+                        onChange={() => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
 
                 <Button type="submit" variant="primary" className="mt-3">
-                    Sign In
+                    Sign Up
                 </Button>
 
                 <Row className="py-3">
                     <Col>
-                        New user? <Link to="/register">Register</Link>
+                        Have an account? <Link to="/login">Login</Link>
                     </Col>
                 </Row>
             </Form>
@@ -49,4 +72,4 @@ const LoginScreen = () => {
     );
 };
 
-export default LoginScreen;
+export default RegisterScreen;
