f97419e8a049d0d70b2e56b83e2b720aa2d4723f
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-29 17:53:45 -0500

    resturctured folders

--- redux06/increment-decrement-number/src/features/counter/counterSlice.js
+++ redux06/increment-decrement-number/src/features/counter/counterSlice.js
@@ -14,9 +14,16 @@ export const counterSlice = createSlice({
         decrement: (state) => {
             state.count -= 1;
         },
+        reset: (state) => {
+            state.count = initialState.count;
+        },
+        incrementByAmount: (state, action) => {
+            state.count += action.payload;
+        },
     },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmount } =
+    counterSlice.actions;
 
 export default counterSlice.reducer;
